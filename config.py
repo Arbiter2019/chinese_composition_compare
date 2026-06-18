dc509ceae9c8fa25725cacb444390e8da13507d9
Arbiter2019 <ecthelll@163.com>
2026-06-18 13:22:27 +0800

    new feature: batch_plagiarism alpeha version, support zh only

--- config.py
+++ config.py
@@ -6,27 +6,12 @@ BATCH_TOP_N = 50
 
 DB_CONFIG_BY_ENV = {
     "dev": {
-        "MYSQL_HOST": "10.1.92.25",
-        "MYSQL_PORT": 3306,
-        "MYSQL_USER": "product",
-        "MYSQL_PASSWORD": "product321",
-        "MYSQL_DB": "platform",
-        "MYSQL_CHARSET": "utf8mb4",
+        "your db config"
     },
     "uat": {
-        "MYSQL_HOST": "47.101.222.159",
-        "MYSQL_PORT": 3310,
-        "MYSQL_USER": "uat_ai",
-        "MYSQL_PASSWORD": "ACLSnkg4HNyt",
-        "MYSQL_DB": "platform_uat",
-        "MYSQL_CHARSET": "utf8mb4",
+         "your db config"
     },
     "prod": {
-        "MYSQL_HOST": "jhpy.rwlb.rds.aliyuncs.com",
-        "MYSQL_PORT": 3306,
-        "MYSQL_USER": "reader",
-        "MYSQL_PASSWORD": "nP42UJkmp5oYWRhN",
-        "MYSQL_DB": "super_campus",
-        "MYSQL_CHARSET": "utf8mb4",
+         "your db config"
     },
 }
